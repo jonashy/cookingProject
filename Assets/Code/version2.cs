1e8657b9f5b91c33095f995cb971c5a02e0560fd
Ricardo <ricardo.sabino@accedo.tv>
2024-03-28 02:29:14 -0400

    - Added receipes loading and fixed ingredients and receipes lists

--- Assets/Code/version2.cs
+++ Assets/Code/version2.cs
@@ -4,10 +4,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Linq;
 
 public class version2 : MonoBehaviour
 {
-    Dictionary<string, List<string>> _allIngredients = new Dictionary<string, List<string>>();
+    List<string> _allIngredients = new List<string>();
+    Dictionary<string, List<string>> _ingredients = new Dictionary<string, List<string>>();
+    Dictionary<string, List<string>> _receipes = new Dictionary<string, List<string>>();
+
     [SerializeField]
     TMP_InputField _searchBox;
     [SerializeField]
@@ -32,19 +36,71 @@ public class version2 : MonoBehaviour
             if(values.Length == 2)
             {
                 var type = values[0];
-                var name = values[1];
+                var ingredient = values[1];
 
-                if(_allIngredients.ContainsKey(type))
+                if(_ingredients.ContainsKey(type))
                 {
-                    _allIngredients[type].Add(name);
+                    _ingredients[type].Add(ingredient);
+                    _allIngredients.Add(ingredient);
+                    print("adding: " + ingredient);
                 }
                 else
                 {
-                    _allIngredients.Add(type, new List<string> { name });
+                    _ingredients.Add(type, new List<string> { ingredient });
+                    _allIngredients.Add(ingredient);
+                    print("adding: " + ingredient);
                 }
             }
         }
 
+        ta = Resources.Load<TextAsset>("Recipes");
+        text = ta.text;
+        lines = text.Split('\n');
+        for (var i = 0; i < lines.Length; i++)
+        {
+            var line = lines[i];
+            var values = line.Split('_');
+            if (values.Length == 2)
+            {
+                var receipe = values[0];
+                var ingredientsStr = values[1];
+                var ingredients = ingredientsStr.Split(',');
+
+                for (var j = 0; j < ingredients.Length; j++)
+                {
+                    var ingredient = ingredients[j];
+
+                    if (_receipes.ContainsKey(receipe))
+                    {
+                        if(_receipes[receipe].Contains(ingredient))
+                        {
+                            Debug.LogWarning("Duplicate ingredient: " + ingredient + " on receipe: " + receipe + " -- line: " + (i + 1));
+                        }
+                        else
+                        {
+                            if (_allIngredients.FirstOrDefault(stringToCheck => stringToCheck.Contains(ingredient)) == null)
+                            {
+                                Debug.LogError("Ingredient " + ingredient + " not found! Receipe: " + receipe + " -- line: " + (i + 1));
+                            }
+
+                            _receipes[receipe].Add(ingredient);
+                        }
+                    }
+                    else
+                    {
+                        if(_allIngredients.FirstOrDefault(stringToCheck => stringToCheck.Contains(ingredient)) == null)
+                        {
+                            Debug.LogError("Ingredient " + ingredient + " not found! Receipe: " + receipe + " -- line: " + (i + 1));
+                        }
+
+                        _receipes.Add(receipe, new List<string> { ingredient });
+                    }
+                }
+            }
+        }
+
+        print("ingredients: " + _allIngredients.Count);
+        print("receipes: " + _receipes.Count);
     }
 
     // Update is called once per frame
@@ -58,9 +114,9 @@ public class version2 : MonoBehaviour
         var value = _searchBox.text;
         print("value: " + value);
 
-        foreach(var key in _allIngredients.Keys)
+        foreach(var key in _ingredients.Keys)
         {
-            if(_allIngredients[key].IndexOf(value) >= 0)
+            if(_ingredients[key].IndexOf(value) >= 0)
             {
                 _ingredientButton.gameObject.SetActive(true);
                 _ingredientButton.GetComponentInChildren<TextMeshProUGUI>().text = value;
