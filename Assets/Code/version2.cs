b1bf762747e497e9a078b3831dc8262336e97d3c
Ricardo <ricardo.sabino@accedo.tv>
2024-04-03 03:07:43 -0400

    Working version with filters

--- Assets/Code/version2.cs
+++ Assets/Code/version2.cs
@@ -4,10 +4,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Linq;
+using System;
 
 public class version2 : MonoBehaviour
 {
-    Dictionary<string, List<string>> _allIngredients = new Dictionary<string, List<string>>();
+    enum Books
+    {
+        All = 0,
+        EWB = 1,
+        CBC = 2
+    }
+
+    List<string> _allIngredients = new List<string>();
+    Dictionary<string, List<string>> _ingredients = new Dictionary<string, List<string>>();
+    Dictionary<string, List<string>> _recipes = new Dictionary<string, List<string>>();
+
+    List<string> _curIngredients = new List<string>();
+    KeyValuePair<string, List<string>> _curRecipe;
+    string _curBook;
+
+    [SerializeField]
+    TMP_Dropdown _bookDropdown;
     [SerializeField]
     TMP_InputField _searchBox;
     [SerializeField]
@@ -15,8 +33,14 @@ public class version2 : MonoBehaviour
     [SerializeField]
     GameObject _ingredientBtnPrefab;
 
+    #region Panels
     [SerializeField]
     GameObject _pantryPanel;
+    [SerializeField]
+    GameObject _availableRecipesPanel;
+    [SerializeField]
+    GameObject _missingIngredientsPanel;
+    #endregion
 
 
     // Start is called before the first frame update
@@ -25,26 +49,78 @@ public class version2 : MonoBehaviour
         var ta = Resources.Load<TextAsset>("Ingredients");
         var text = ta.text;
         var lines = text.Split('\n');
-        for(var i = 0; i < lines.Length; i++)
+        for (var i = 0; i < lines.Length; i++)
         {
-            var line = lines[i];
+            var line = lines[i].Trim();
             var values = line.Split('_');
-            if(values.Length == 2)
+            if (values.Length == 2)
             {
-                var type = values[0];
-                var name = values[1];
+                var type = values[0].Trim();
+                var ingredient = values[1].Trim();
 
-                if(_allIngredients.ContainsKey(type))
+                if (_ingredients.ContainsKey(type))
                 {
-                    _allIngredients[type].Add(name);
+                    _ingredients[type].Add(ingredient);
+                    _allIngredients.Add(ingredient);
+                    print("adding: " + ingredient);
                 }
                 else
                 {
-                    _allIngredients.Add(type, new List<string> { name });
+                    _ingredients.Add(type, new List<string> { ingredient });
+                    _allIngredients.Add(ingredient);
+                    print("adding: " + ingredient);
                 }
             }
         }
 
+        ta = Resources.Load<TextAsset>("Recipes");
+        text = ta.text;
+        lines = text.Split('\n');
+        for (var i = 0; i < lines.Length; i++)
+        {
+            var line = lines[i].Trim();
+            var values = line.Split('_');
+            if (values.Length == 2)
+            {
+                var receipe = values[0].Trim();
+                var ingredientsStr = values[1].Trim();
+                var ingredients = ingredientsStr.Split(',');
+
+                for (var j = 0; j < ingredients.Length; j++)
+                {
+                    var ingredient = ingredients[j].Trim();
+
+                    if (_recipes.ContainsKey(receipe))
+                    {
+                        if (_recipes[receipe].Contains(ingredient))
+                        {
+                            Debug.LogWarning("Duplicate ingredient: " + ingredient + " on receipe: " + receipe + " -- line: " + (i + 1));
+                        }
+                        else
+                        {
+                            if (_allIngredients.FirstOrDefault(stringToCheck => stringToCheck.Contains(ingredient)) == null)
+                            {
+                                Debug.LogError("Ingredient " + ingredient + " not found! Receipe: " + receipe + " -- line: " + (i + 1));
+                            }
+
+                            _recipes[receipe].Add(ingredient);
+                        }
+                    }
+                    else
+                    {
+                        if (_allIngredients.FirstOrDefault(stringToCheck => stringToCheck.Contains(ingredient)) == null)
+                        {
+                            Debug.LogError("Ingredient " + ingredient + " not found! Receipe: " + receipe + " -- line: " + (i + 1));
+                        }
+
+                        _recipes.Add(receipe, new List<string> { ingredient });
+                    }
+                }
+            }
+        }
+
+        print("ingredients: " + _allIngredients.Count);
+        print("receipes: " + _recipes.Count);
     }
 
     // Update is called once per frame
@@ -58,9 +134,10 @@ public class version2 : MonoBehaviour
         var value = _searchBox.text;
         print("value: " + value);
 
-        foreach(var key in _allIngredients.Keys)
+        foreach(var ingredient in _allIngredients)
         {
-            if(_allIngredients[key].IndexOf(value) >= 0)
+            print(ingredient + " -- " + value + "======" + (ingredient.Equals(value)));
+            if (ingredient.Equals(value))
             {
                 _ingredientButton.gameObject.SetActive(true);
                 _ingredientButton.GetComponentInChildren<TextMeshProUGUI>().text = value;
@@ -74,14 +151,101 @@ public class version2 : MonoBehaviour
 
     }
 
-    public void OnAddToPantryButtonClick()
+    private void AddIngredientToMissingIngredientsPanel(string ingredient)
     {
         var go = Instantiate(_ingredientBtnPrefab, Vector3.zero, Quaternion.identity);
-        go.GetComponentInChildren<TextMeshProUGUI>().text = _searchBox.text;
-        go.GetComponent<RectTransform>().position = Vector3.zero;
-        go.transform.parent = _pantryPanel.transform;
+        go.GetComponentInChildren<TextMeshProUGUI>().text = ingredient;
+        go.transform.SetParent(_missingIngredientsPanel.transform);
+        go.GetComponentInChildren<RectTransform>().position = Vector3.zero;
+    }
+
+    private void AddRecipeToAvailableRecipesPanel(KeyValuePair<string, List<string>> recipe)
+    {
+        var go = Instantiate(_ingredientBtnPrefab, Vector3.zero, Quaternion.identity);
+        go.GetComponentInChildren<Button>().onClick.AddListener(() =>
+        {
+            RemoveAllItemsFromGameObject(_missingIngredientsPanel);
+
+            // Find missing ingredients from current recipe
+            _curRecipe = recipe;
+            
+            var missingList = recipe.Value.Where(i => !_curIngredients.Contains(i)).ToList();
+            foreach(var ingredient in missingList)
+            {
+                AddIngredientToMissingIngredientsPanel(ingredient);
+            }
+        });
+        go.GetComponentInChildren<TextMeshProUGUI>().text = recipe.Key;
+        go.transform.SetParent(_availableRecipesPanel.transform);
+        go.GetComponentInChildren<RectTransform>().position = Vector3.zero;
     }
 
 
+
+    public void OnAddToPantryButtonClick()
+    {
+        var ingredient = _searchBox.text;
+
+        if(_curIngredients.Contains(ingredient))
+        {
+            return;
+        }
+
+        var go = Instantiate(_ingredientBtnPrefab, Vector3.zero, Quaternion.identity);
+        go.GetComponentInChildren<TextMeshProUGUI>().text = ingredient;
+        go.transform.SetParent(_pantryPanel.transform);
+        go.GetComponentInChildren<RectTransform>().position = Vector3.zero;
+        go.GetComponentInChildren<Button>().onClick.AddListener(() =>
+        {
+            _curIngredients.Remove(ingredient);
+            DestroyImmediate(go);
+            UpdateListOfRecipes();
+            RemoveAllItemsFromGameObject(_missingIngredientsPanel);
+        });
+
+        _curIngredients.Add(ingredient);
+
+        UpdateListOfRecipes();
+    }
+
+    private void UpdateListOfRecipes()
+    {
+        // Find recipes that match all the ingredients in the pantry
+        var recipes = _recipes.Where(v => {
+            if (String.IsNullOrEmpty(_curBook) || _curBook == "All")
+            {
+                return _curIngredients.Count > 0 &&
+                   v.Value.Intersect(_curIngredients).Count() == _curIngredients.Count;
+            }
+
+            return _curIngredients.Count > 0 && v.Key.Contains(_curBook) &&
+               v.Value.Intersect(_curIngredients).Count() == _curIngredients.Count;
+            }
+        ).ToList();
+        print("Found: " + recipes.Count + " recipes!");
+        RemoveAllItemsFromGameObject(_availableRecipesPanel);
+        foreach (var recipe in recipes)
+        {
+            AddRecipeToAvailableRecipesPanel(recipe);
+        }
+    }
+
+    private void RemoveAllItemsFromGameObject(GameObject go)
+    {
+        var t = go.transform;
+        while(t.childCount > 0)
+        {
+            var child = t.GetChild(0);
+            child.SetParent(null);
+            DestroyImmediate(child.gameObject);
+        }
+    }
+
+    public void OnBookDropDownChange()
+    {
+        _curBook = Enum.GetName(typeof(Books), _bookDropdown.value);
+        UpdateListOfRecipes();
+        RemoveAllItemsFromGameObject(_missingIngredientsPanel);
+    }
 
 }
